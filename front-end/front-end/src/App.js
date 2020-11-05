87a058dc3acd6d0e0d26d1977520faba139a2041
AlaaRaedDawood <you@example.com>
2020-11-05 10:59:33 +0200

    front-end start

--- front-end/front-end/src/App.js
+++ front-end/front-end/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {Container} from'reactstrap'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <div className="App">
-      
-          Hello Alaa Raed
+         <Container>
+         <Login></Login>
+         <Dashboard/>
+         </Container>
+        
       
     </div>
   );
