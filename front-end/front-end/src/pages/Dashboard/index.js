c1e751d58fd53a7842bca89179893ac40f5dc83e
AlaaRaedDawood <you@example.com>
2020-12-24 18:10:48 +0200

    dashboard is begining to shape

--- front-end/front-end/src/pages/Dashboard/index.js
+++ front-end/front-end/src/pages/Dashboard/index.js
@@ -1,6 +1,24 @@
-import React from 'react' ;
+import React , {useState , useEffect}from 'react' ;
+import api from '../../Services/api';
 
-function dashboard (){
+ function Dashboard (){
+    const [filter,setFilter] = useState('');
+    const [event,setEvent] = useState([]);
+    const user_id = localStorage.getItem('user');
+    console.log(user_id);
+    const getEvents = async () => {
+        const url = filter ? `/dashboard/${filter}` : '/dashboard' ;
+        const response =  await api.get(url , { headers : {user_id}}) ;
+        
+        response.data && setEvent(response.data) ; 
+        console.log(event) ;
+    }
+    
+
+   
+    useEffect( 
+    getEvents
+    ,[]);
     return (
         <div>
          hello from dashboard 
@@ -9,4 +27,4 @@ function dashboard (){
 
 }
 
-export default dashboard ;
+ export default Dashboard ;
